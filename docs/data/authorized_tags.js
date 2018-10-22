89608dd088160f3ffa6720fd5934cb5213fed3ef
Saminu <s.salisu@live.co.uk>
2018-10-22 14:46:56 +0100

    WIP

---
+++ docs/data/authorized_tags.js
@@ -0,0 +1,173 @@
+define({
+	authorizedTags: {
+		"object_type:image": {
+			"description": "Image"
+		},
+		"object_type:text": {
+			"description": "Text"
+		},
+		"object_type:video": {
+			"description": "Video"
+		},
+		"object_type:3d_object": {
+			"description": "3D object (e.g., sculture, coin, jewel...)"
+		},
+		"object_type:audio": {
+			"description": "Audio"
+		},
+		"data:time": {
+			"description": "Time"
+		},
+		"data:place": {
+			"description": "Place"
+		},
+		"data:category": {
+			"description": "Category"
+		},
+		"data:relation": {
+			"description": "Relation"
+		},
+		"data:actor": {
+			"description": "Actor"
+		},
+		"data:text": {
+			"description": "Text"
+		},
+		"data:other": {
+			"description": "Other"
+		},
+		"data:ontology": {
+			"description": "Ontology"
+		},
+		"data:object": {
+			"description": "The visualisation shows a visual representation of the object itself, along with its metadata "
+		},
+		"data:hierarchy": {
+			"description": "Hierarchy"
+		},
+		"user:casual": {
+			"description": "Casual user"
+		},
+		"user:professional": {
+			"description": "Professional user"
+		},
+		"evaluation:yes": {
+			"description": "The approach was validated (e.g. by user study, usability test, ..."
+		},
+		"task:elementary": {
+			"description": "Elementary task"
+		},
+		"task:synoptic": {
+			"description": "Synoptic task"
+		},
+		"task:temporal": {
+			"description": "Synoptic task focusing on temporal patterns"
+		},
+		"granularity:single-object_preview": {
+			"description": "Preview of a single object"
+		},
+		"granularity:multi-object_preview": {
+			"description": "Preview of multiple objects"
+		},
+		"granularity:abstract_overview": {
+			"description": "Abstract overview"
+		},
+		"granularity:discrete_overview": {
+			"description": "Discrete overview"
+		},
+		"interactivity:analysis_and_orientation": {
+			"description": "Analysis and orientation"
+		},
+		"interactivity:detail": {
+			"description": "Detail"
+		},
+		"interactivity:horizontal_browsing": {
+			"description": "Horizontal browsing"
+		},
+		"interactivity:vertical_immersion": {
+			"description": "Vertical immersion"
+		},
+		"interactivity:search": {
+			"description": "Search"
+		},
+		"interactivity:curated_paths": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:list_and_slideshow": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:maps": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:networks": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:grid_and_mosaic": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:other": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:barchart": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:wordcloud": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:3d": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:plot": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:clusters": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:treemaps": {
+			"description": "Curated paths"
+		},
+		"non-temporal_encoding:sets": {
+			"description": "Curated paths"
+		},
+		"temporal_encoding:time_axis(2d)": {
+			"description": "Curated paths"
+		},
+		"temporal_encoding:timeline(1d)": {
+			"description": "Curated paths"
+		},
+		"temporal_encoding:other": {
+			"description": "Curated paths"
+		},
+		"temporal_encoding:none": {
+			"description": "Curated paths"
+		},
+		"temporal_encoding:animation": {
+			"description": "Curated paths"
+		},
+		"temporal_encoding:retinal_variable": {
+			"description": "Curated paths"
+		},
+		"temporal_encoding:time_axis(3d)": {
+			"description": "Curated paths"
+		},
+		"source:paper": {
+			"description": "A scientific publication"
+		},
+		"source:tool": {
+			"description": "A prototype/tool/application without an accompanying scientific publication"
+		},
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ docs/data/tag_categories.js
@@ -0,0 +1,34 @@
+define({
+	tagCategories: {
+		"object_type": {
+			"description": "CH object type"
+		},
+		"data": {
+			"description": "Data aspect"
+		},
+		"user": {
+			"description": "User type"
+		},
+		"evaluation": {
+			"description": "Evaluation"
+		},
+		"task": {
+			"description": "Supported task"
+		},
+		"granularity": {
+			"description": "Granularity"
+		},
+		"interactivity": {
+			"description": "Interactivity"
+		},
+		"non-temporal_encoding": {
+			"description": "Visual encoding of non-temporal data"
+		},
+		"temporal_encoding": {
+			"description": "Visual encoding of temporal data"
+		},
+		"source": {
+			"description": "Source"
+		}
+	}
+});
