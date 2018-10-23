7f8c907704d099ecd525a02505ef51f1ffcf1d04
Saminu <s.salisu@live.co.uk>
2018-10-23 10:54:25 +0100

    WIP

--- docs/properties.js
+++ docs/properties.js
@@ -61,7 +61,7 @@ var paper = {
     // html: 'Link to the Survey  <b><a href="//mariandoerk.de/papers/tvcg2018.pdf" target="_blank">(TVCG 2018)</a></b>',
     // html: 'Link to the Survey  <b><a href="https://github.com/danubevislab/collectionvis/raw/master/data/tvcg2018.pdf" target="_blank">(TVCG 2018)</a></b>',
     // html: 'Link to the Survey  <b><a href="https://www.scribd.com/document/391404190/tvcg2018#fullscreen=1" target="_blank">(TVCG 2018)</a></b>',
-    html: 'Link to the Survey  <b><a href="https://drive.google.com/file/d/18RaqB20zJpRLtQiNkBp0yuJz16te7jSd/view?usp=sharing" target="_blank">(TVCG 2018)</a></b>',
+    html: 'Link to the Survey  <b><a href="https://publik.tuwien.ac.at/files/publik_269483.pdf" target="_blank">(TVCG 2018)</a></b>',
     id: null
 };
 
