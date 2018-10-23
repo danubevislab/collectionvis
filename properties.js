9fbc597168ae0d90f79e735b30f545badfa11ffa
Saminu <s.salisu@live.co.uk>
2018-10-23 09:40:33 +0100

    WIP

--- properties.js
+++ properties.js
@@ -59,7 +59,8 @@ var editable = false;
 var paper = {
     // html: 'Link to the Survey  <b><a href="https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=8352050" target="_blank">(TVCG 2018)</a></b>',
     // html: 'Link to the Survey  <b><a href="//mariandoerk.de/papers/tvcg2018.pdf" target="_blank">(TVCG 2018)</a></b>',
-    html: 'Link to the Survey  <b><a href="https://github.com/danubevislab/collectionvis/raw/master/data/tvcg2018.pdf" target="_blank">(TVCG 2018)</a></b>',
+    // html: 'Link to the Survey  <b><a href="https://github.com/danubevislab/collectionvis/raw/master/data/tvcg2018.pdf" target="_blank">(TVCG 2018)</a></b>',
+    html: 'Link to the Survey  <b><a href="https://www.scribd.com/document/391404190/tvcg2018#fullscreen=1" target="_blank">(TVCG 2018)</a></b>',
     id: null
 };
 
